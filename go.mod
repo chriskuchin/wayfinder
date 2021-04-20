ad210c5b92762f6242bf93766fe66a2cb6a12bff
Chris Kuchin <chris.kuchin@neuro-id.com>
2021-04-19 19:09:32 -0600

    adding support for route53

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module chriskuchin/dns
 go 1.14
 
 require (
+	github.com/aws/aws-sdk-go v1.27.0
 	github.com/cloudflare/cloudflare-go v0.12.2
 	github.com/hashicorp/consul/api v1.5.0
+	github.com/prometheus/common v0.20.0
 	github.com/urfave/cli/v2 v2.2.0
 )
--- main.go
+++ main.go
@@ -7,15 +7,22 @@ import (
 	"os"
 	"strings"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/route53"
 	"github.com/cloudflare/cloudflare-go"
 	"github.com/hashicorp/consul/api"
+	"github.com/prometheus/common/log"
 	"github.com/urfave/cli/v2"
 )
 
 type (
 	// DNSRecord represents needed info to update or create a DNS Record
 	DNSRecord struct {
+		ID          string
 		Type        string
+		Content     string
+		Proxied     bool
 		Name        string
 		Destination string
 		TTL         int
@@ -24,11 +31,12 @@ type (
 )
 
 var (
-	consulURL        string
-	cloudflareToken  string
-	cloudflareZoneID string
+	consulURL       string
+	cloudflareToken string
+	zoneID          string
+	dryRun          bool
 
-	cloudflareBaseURL = "https://api.cloudflare.com/client/v4/"
+	DNSProvider string
 
 	publicIP string
 )
@@ -38,13 +46,22 @@ func main() {
 		Name:  "boom",
 		Usage: "make an explosive entrance",
 		Action: func(c *cli.Context) error {
+			DNSProvider = "cloudflare"
+			if cloudflareToken == "" {
+				DNSProvider = "route53"
+			}
+
 			publicIP = getPublicIP()
-			existingZoneRecords := getZoneRecords(cloudflareZoneID)
+			existingZoneRecords := getZoneRecords(zoneID)
+			if existingZoneRecords == nil {
+				return fmt.Errorf("failed to retrieve zone records")
+			}
 
 			config := api.DefaultConfig()
 			config.Address = consulURL
 			client, err := api.NewClient(config)
 			if err != nil {
+				log.Error(err)
 				panic(err)
 			}
 
@@ -52,24 +69,25 @@ func main() {
 
 			services, _, err := catalog.Services(&api.QueryOptions{})
 			if err != nil {
+				log.Error(err)
 				return err
 			}
 
 			for service, tags := range services {
 				settings := getSettings(tags)
 				if len(settings) > 0 {
-					record := cloudflare.DNSRecord{
+					record := DNSRecord{
 						Type: "A",
 						TTL:  1,
 					}
 					for _, setting := range settings {
 						settingPieces := strings.Split(setting, "=")
-						if settingPieces[0] == "wayfinder.domain" {
+						if strings.HasPrefix(settingPieces[0], "wayfinder.domain") {
 							record.Name = settingPieces[1]
-						} else if settingPieces[0] == "wayfinder.public" && settingPieces[1] == strings.ToLower("true") {
+						} else if strings.HasPrefix(settingPieces[0], "wayfinder.public") && settingPieces[1] == strings.ToLower("true") {
 							record.Content = publicIP
 							record.Proxied = true
-						} else if settingPieces[0] == "wayfinder.address" {
+						} else if strings.HasPrefix(settingPieces[0], "wayfinder.address") {
 							record.Content = settingPieces[1]
 						}
 					}
@@ -82,23 +100,27 @@ func main() {
 					currentRecord := existingZoneRecords[record.Name]
 
 					if currentRecord.Name == "" || currentRecord.Content != record.Content || currentRecord.TTL != record.TTL || currentRecord.Type != record.Type {
-						fmt.Println("records Don't match")
-						fmt.Printf("%#v\n", currentRecord)
-						fmt.Printf("%#v\n", record)
-						if currentRecord.Name != "" {
-							fmt.Println("Delete Record: ", currentRecord.ID)
-							deleteZoneRecord(currentRecord.ID, cloudflareZoneID)
+						log.Info("records Don't match")
+						log.Infof("%#v\n", currentRecord)
+						log.Infof("%#v\n", record)
+						if !dryRun {
+							updateZoneRecord(record, zoneID)
 						}
-						fmt.Println("Create Record: ", record)
-						createZoneRecord(record, cloudflareZoneID)
 					} else {
-						fmt.Println("Record matches no need to update: ", service)
+						log.Info("Record matches no need to update: ", service)
 					}
 				}
 			}
 			return nil
 		},
 		Flags: []cli.Flag{
+			&cli.BoolFlag{
+				Name:        "dry-run",
+				Value:       false,
+				Destination: &dryRun,
+				EnvVars:     []string{},
+				Aliases:     []string{},
+			},
 			&cli.StringFlag{
 				Name:        "consul-url",
 				Value:       "http://consul.service.consul:8500",
@@ -110,27 +132,18 @@ func main() {
 			&cli.StringFlag{
 				Name:        "cloudflare-api-key",
 				Destination: &cloudflareToken,
-				Required:    true,
+				Required:    false,
 				EnvVars: []string{
 					"CLOUDFLARE_API_TOKEN",
 				},
 			},
 			&cli.StringFlag{
-				Name:        "cloudflare-zone-id",
-				Destination: &cloudflareZoneID,
+				Name:        "zone-id",
+				Destination: &zoneID,
 				Required:    true,
 				EnvVars: []string{
 					"CLOUDFLARE_ZONE_ID",
-				},
-			},
-		},
-		Commands: []*cli.Command{
-			{
-				Name:  "sync",
-				Flags: []cli.Flag{},
-				Action: func(c *cli.Context) error {
-
-					return nil
+					"ZONE_ID",
 				},
 			},
 		},
@@ -161,49 +174,139 @@ func getPublicIP() string {
 	return strings.TrimSpace(string(body))
 }
 
-func getZoneRecords(zoneID string) map[string]cloudflare.DNSRecord {
-	cf, err := cloudflare.NewWithAPIToken(cloudflareToken)
-	if err != nil {
-		fmt.Println(err)
-		return nil
-	}
+func getZoneRecords(zoneID string) map[string]DNSRecord {
+	result := map[string]DNSRecord{}
+	if DNSProvider == "route53" {
+		svc := route53.New(session.New())
+		out, err := svc.ListResourceRecordSets(&route53.ListResourceRecordSetsInput{
+			HostedZoneId: aws.String(zoneID),
+		})
+		if err != nil {
+			log.Error(err)
+		}
 
-	records, err := cf.DNSRecords(zoneID, cloudflare.DNSRecord{Type: "A"})
-	if err != nil {
-		fmt.Println(err)
-		return nil
-	}
+		for _, record := range out.ResourceRecordSets {
+			contents := []string{}
+			for _, content := range record.ResourceRecords {
+				contents = append(contents, *content.Value)
+			}
 
-	result := map[string]cloudflare.DNSRecord{}
-	for _, record := range records {
-		fmt.Println(record.Name)
-		result[record.Name] = record
-	}
+			result[strings.TrimSuffix(*record.Name, ".")] = DNSRecord{
+				Name:    *record.Name,
+				TTL:     int(*record.TTL),
+				Type:    *record.Type,
+				Content: strings.Join(contents, ", "),
+			}
+		}
 
-	return result
+		return result
+
+	} else {
+		cf, err := cloudflare.NewWithAPIToken(cloudflareToken)
+		if err != nil {
+			log.Errorf("Failed to create new cloudflare client: %+v", err)
+			return nil
+		}
+
+		records, err := cf.DNSRecords(zoneID, cloudflare.DNSRecord{Type: "A"})
+		if err != nil {
+			log.Errorf("Failed to query zone for existing records: %+v")
+			return nil
+		}
+
+		for _, record := range records {
+			result[record.Name] = DNSRecord{
+				ID:      record.ID,
+				Name:    record.Name,
+				TTL:     record.TTL,
+				Type:    record.Type,
+				Content: record.Content,
+			}
+		}
+
+		return result
+
+	}
 }
 
-func createZoneRecord(record cloudflare.DNSRecord, zoneID string) {
+func updateZoneRecord(record DNSRecord, zoneID string) {
+	if DNSProvider == "route53" {
+		svc := route53.New(session.New())
+
+		resourceRecords := []*route53.ResourceRecord{}
+
+		for _, resRecord := range strings.Split(record.Content, ", ") {
+			resourceRecords = append(resourceRecords, &route53.ResourceRecord{
+				Value: aws.String(resRecord),
+			})
+		}
+		input := &route53.ChangeResourceRecordSetsInput{
+			ChangeBatch: &route53.ChangeBatch{
+				Changes: []*route53.Change{
+					{
+						Action: aws.String("UPSERT"),
+						ResourceRecordSet: &route53.ResourceRecordSet{
+							Name:            aws.String(record.Name),
+							ResourceRecords: resourceRecords,
+							TTL:             aws.Int64(int64(record.TTL)),
+							Type:            aws.String(record.Type),
+						},
+					},
+				},
+				Comment: aws.String("Wayfinder Managed Domain"),
+			},
+			HostedZoneId: aws.String(zoneID),
+		}
+
+		result, err := svc.ChangeResourceRecordSets(input)
+		if err != nil {
+			log.Error(err)
+			return
+		}
+
+		log.Info(result)
+
+	} else if DNSProvider == "cloudflare" {
+		if record.Name != "" {
+			log.Info("Delete Cloudflare Record: ", record.Name)
+			if !dryRun {
+				deleteCloudflareZoneRecord(record.ID, zoneID)
+			}
+		}
+		log.Info("Create Cloudflare Record: ", record)
+		if !dryRun {
+			createCloudflareZoneRecord(record, zoneID)
+		}
+	}
+}
+
+func createCloudflareZoneRecord(record DNSRecord, zoneID string) {
 	cf, err := cloudflare.NewWithAPIToken(cloudflareToken)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	res, err := cf.CreateDNSRecord(zoneID, record)
+	res, err := cf.CreateDNSRecord(zoneID, cloudflare.DNSRecord{
+		Type:    record.Type,
+		TTL:     record.TTL,
+		Proxied: record.Proxied,
+		Content: record.Content,
+		Name:    record.Name,
+	})
 	if err != nil {
-		fmt.Println(err)
+		log.Error(err)
 		return
 	}
 
-	fmt.Println(res)
+	log.Info(res)
 }
 
-func deleteZoneRecord(id, zoneID string) {
+func deleteCloudflareZoneRecord(id, zoneID string) {
 	cf, err := cloudflare.NewWithAPIToken(cloudflareToken)
 	if err != nil {
-		fmt.Println(err)
-		// return nil
+		log.Error(err)
+		return
 	}
 
 	cf.DeleteDNSRecord(zoneID, id)
